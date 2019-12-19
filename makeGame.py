9c320f3f8a9f75e49033f6c946ce14aa27702821
Simon Katan <s.katan@gold.ac.uk>
2019-12-19 20:06:08 +0000

    first pass

--- makeGame.py
+++ makeGame.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 from PIL import Image, ImageDraw, ImageFont
 import math
+from random import randint
+from random import shuffle
+import json
 
 phrases = [
 "Splendid occasion",
@@ -21,6 +24,9 @@ phrases = [
 ]
 
 phrase_x = "but I do wonder when we'll be eating..."
+
+#################################### HELPER FUNCTIONS ####################################
+
 
 def find_all(a_str, sub):
     start = 0
@@ -50,77 +56,127 @@ def wrapText(text, font, w):
         elif s[0] < best_size:
             best_text = t_text
             best_size = s[0]
-    print(best_text)
     return wrapText(best_text, font, w)
 
+def choose(items):
+    i = randint(0, len(items)-1)
+    return items[i]
 
+#################################### DRAWING ####################################
+
+def drawMappings(draw, table):
+    table_top = height/8
+    table_bottom = height * 15/16
+
+    draw.line((width/2, 0, width/2, height), fill=100)
+    draw.line((width * 3/4, table_top, width * 3/4, table_bottom), fill=0)
+
+    o = centerText("They say", normal_fnt, width/4)
+    draw.text((width/2 + o,table_top), "They say", font=normal_fnt, fill=(0))
+    o = centerText(" ... you say", normal_fnt, width/4)
+    draw.text((width * 3/4 + o,table_top), " ... you say", font=normal_fnt, fill=(0))
+
+    draw.line((width/2, table_top + normal_size, width, table_top + normal_size), fill=100)
+
+    #draw the they say phrases
+    running_y = table_top + normal_size
+    offset_x = 10
+
+    #mappings
+    keys = list(table["mappings"].keys())
+    keys = list(keys)
+    shuffle(keys)
+
+    for i in keys:
+        t = wrapText(data["phrases"][i], normal_fnt, width/4 - offset_x * 2)
+        s = normal_fnt.getsize_multiline(t)
+        v = s[1]
+        draw.text((width/2 + offset_x,running_y), t, font=normal_fnt, fill=(0))
+        if i == "x":
+            answer = "Well I've heard that you're sitting on table " + str(table["mappings"]["x"])
+        else:
+            answer = table["mappings"][i]
+            if isinstance(answer, list):
+                answer = choose(answer)
+            answer = data["phrases"][answer]
+        t = wrapText(answer, normal_fnt, width/4 - offset_x * 2)
+        s = normal_fnt.getsize_multiline(t)
+        v = max(s[1], v)
+        draw.text((width *3/4 + offset_x,running_y), t, font=normal_fnt, fill=(0))
+        running_y += v + normal_size
+        draw.line((width/2, running_y, width, running_y), fill=100)
+
+    draw.text((width/2 + offset_x,running_y), " ? ? ? ", font=normal_fnt, fill=(0))
+    t = wrapText(table["excuse"], normal_fnt, width/4 - offset_x * 2)
+    draw.text((width *3/4 + offset_x,running_y), t, font=normal_fnt, fill=(0))
+
+#################################### MAIN ####################################
+
+def drawCard(person, table):
+
+    #prepare the image
+    im  = Image.new('RGB', (width,height), (255,255,255))
+
+    # get a drawing context
+    draw = ImageDraw.Draw(im)
+
+
+    ### title page and instructions
+    v = 10
+    t1 = "Simon and Marguerite's"
+    o = centerText(t1, normal_fnt,width/2)
+    draw.text((o,v), t1,font=normal_fnt, fill=(0))
+    s = normal_fnt.getsize(t1)
+    v += s[1]
+
+    t2 = "Conversation Piece"
+    o = centerText(t2, title_fnt,width/2)
+    draw.text((o,v), t2, font=title_fnt, fill=(0))
+    s = title_fnt.getsize(t2)
+    v += s[1]
+    o = centerText(person, normal_fnt ,width/2)
+    draw.text((o,v), person, font=normal_fnt, fill=(0))
+    s = normal_fnt.getsize(t2)
+    v += s[1] + 20
+
+    ### instructions
+
+    draw.text((10,v),instructions,font=small_fnt, fill=(0))
+
+
+    ### second page
+    opener = data["phrases"][table["opener"]]
+    o = centerText("Opener: " + opener, normal_fnt ,width/2)
+    draw.text((o + width/2 ,20), "Opener: " + opener, font=normal_fnt, fill=(0))
+
+    drawMappings(draw, table)
+
+
+    im.save("./output/" + person, "PNG")
+
+################################################################################
 
 print('Wedding game maker !')
 
 res = 1000
 width = math.floor(1.4142857 * res)
 height = res
-im  = Image.new('RGB', (width,height), (255,255,255));
-
-# get a drawing context
-draw = ImageDraw.Draw(im)
-
-
-
 
 # get a font
 title_fnt = ImageFont.truetype('./Chocolat.ttf', 100)
 normal_fnt = ImageFont.truetype('./Alice-regular.ttf', 35)
+small_fnt = ImageFont.truetype('./Alice-regular.ttf', 25)
 normal_size = 35
 
-# draw text, half opacity
+with open('defs.json') as json_file:
+    data = json.load(json_file)
 
-opener = phrases[0]
+with open('people.json') as json_file:
+    people = json.load(json_file)
 
-draw.text((10,10), "Simon and Marguerite's\nConversation Piece", font=title_fnt, fill=(0))
+with open('instructions.txt', "r") as txt_file:
+     instructions = txt_file.read()
 
-o = centerText("Opener: " + opener, normal_fnt ,width/2)
-draw.text((o + width/2 ,20), "Opener: " + opener, font=normal_fnt, fill=(0))
-
-table_top = height/8
-table_bottom = height * 15/16
-
-draw.line((width/2, 0, width/2, height), fill=100)
-draw.line((width * 3/4, table_top, width * 3/4, table_bottom), fill=0)
-
-o = centerText("They say", normal_fnt, width/4)
-draw.text((width/2 + o,table_top), "They say", font=normal_fnt, fill=(0))
-o = centerText(" ... you say", normal_fnt, width/4)
-draw.text((width * 3/4 + o,table_top), " ... you say", font=normal_fnt, fill=(0))
-
-draw.line((width/2, table_top + normal_size, width, table_top + normal_size), fill=100)
-
-#draw the they say phrases
-running_y = table_top + normal_size
-offset_x = 10
-
-for i in range(0,6):
-    t = wrapText(phrases[i], normal_fnt, width/4 - offset_x * 2)
-    s = normal_fnt.getsize_multiline(t)
-    v = s[1]
-    draw.text((width/2 + offset_x,running_y), t, font=normal_fnt, fill=(0))
-    t = wrapText(phrases[i+1], normal_fnt, width/4 - offset_x * 2)
-    s = normal_fnt.getsize_multiline(t)
-    v = max(s[1], v)
-    draw.text((width *3/4 + offset_x,running_y), t, font=normal_fnt, fill=(0))
-    running_y += v + normal_size
-    draw.line((width/2, running_y, width, running_y), fill=100)
-
-
-t = wrapText(phrase_x, normal_fnt, width/4 - offset_x * 2)
-draw.text((width/2 + offset_x,running_y), t, font=normal_fnt, fill=(0))
-t = wrapText("Well I've heard that you're sitting on table " + str(4), normal_fnt, width/4 - offset_x * 2)
-draw.text((width * 3/4 + offset_x,running_y), t, font=normal_fnt, fill=(0))
-
-
-im.show();
-
-
-
-# write to stdout
-# im.save(sys.stdout, "PNG")
+for p in people:
+    d = data["tables"][p[1] - 1]
+    drawCard(p[0],d)
